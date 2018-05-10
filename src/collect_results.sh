4ee8a2b9e27c3dd22d8e54d44b132e669c366ec9
schillic <schillic@informatik.uni-freiburg.de>
2018-05-10 11:04:19 +0200

    update result collection script

--- src/collect_results.sh
+++ src/collect_results.sh
@@ -13,7 +13,55 @@ MODES=( \
        "reach-2D-eps-fixedstep-twovars.txt" \
       )
 
-function collect(){
+function convert_to_scientific() {
+    if [ -z $(echo "$1" | grep "e") ]; then
+        # decimal notation
+        I=$(echo $1 | awk '{print int($0)}')
+        D=$(echo $1 | awk '{print $0-int($0)}' | sed 's/0\.\([0-9]*\)/\1/')
+        if [ $I -eq 0 ]; then
+            # number is 0.XXX
+            EXPONENT=-1
+            while [ ${D:0:1} -eq 0 ]; do
+                D=${D:1}
+                EXPONENT=$(($EXPONENT - 1))
+            done
+            L_D=${#D}
+            if [ $L_D -eq 1 ]; then
+                # one digit remains
+                AFTER="${D:1:1}0"
+            else
+                # at least two digits remain
+                AFTER="${D:1:2}"
+            fi
+            SHARED="${D:0:1}.${AFTER}e$EXPONENT"
+        else
+            # number is bigger than 1
+            L_I=${#I}
+            if [ $L_I -eq 1 ]; then
+                # one-digit integer part
+                AFTER="${D:0:2}"
+                EXPONENT=0
+            elif [ $L_I -eq 2 ]; then
+                # two-digit integer part
+                AFTER="${I:1:1}${D:0:1}"
+                EXPONENT=1
+            else
+                # at least three-digit integer part
+                AFTER=${I:1:2}
+                EXPONENT=$(($L_I - 1))
+            fi
+            SHARED="${I:0:1}.${AFTER}e$EXPONENT"
+        fi
+    else
+        # scientific notation
+        E=$(echo $1 | sed 's/.*e\(.*\)/\1/')
+        # take first 4 characters
+        N=$(echo $1 | sed 's/\(....\).*e.*/\1/')
+        SHARED="${N}e$E"
+    fi
+}
+
+function collect() {
     for MODE in ${MODES[@]}; do
         OUTPUT_FILE="res-$MODE"
         
@@ -35,16 +83,21 @@ function collect(){
             
             # disretization
             COUNTER=1
+            SHARED=""
             for T in $TIMES; do
                 if [ $COUNTER -eq 1 ]; then
                     DISC=$T
+                    convert_to_scientific $T
+                    DISC_SC=$SHARED
                 elif [ $COUNTER -eq 4 ]; then
                     TOTAL=$T
+                    convert_to_scientific $T
+                    TOTAL_SC=$SHARED
                 fi
                 COUNTER=$(($COUNTER + 1))
             done
             
-            echo -e "$MODEL\t$DISC\t$TOTAL" >> "$OUTPUT_FILE"
+            echo -e "$MODEL\t$DISC\t$TOTAL\n\t$DISC_SC\t\t$TOTAL_SC" >> "$OUTPUT_FILE"
         done
     done
 }
@@ -70,22 +123,22 @@ popd > /dev/null
 # # name of the result file
 # RESULT_FILE="results-$(date +%Y%m%d%H%M%S).txt"
 # if [ ! -e $RESULT_FILE ]; then
-# 	touch $RESULT_FILE
+#     touch $RESULT_FILE
 # fi
 # 
 # for EX in ${NAMES[@]}; do
-# 	cd $EX
-# 	if [ ! -e $DATA_FILE ]; then
-# 		echo "no $DATA_FILE file found, skipping $EX..."
-# 		cd ..
-# 		continue
-# 	fi
-# 	echo ---$'\n'Results for model $EX >> "../$RESULT_FILE"
-# 	
-# 	for CATEGORY in "${CATEGORIES[@]}"; do
-# 		TIME=$(grep -A 1 "$CATEGORY" $DATA_FILE | tail -1 | sed -e 's/elapsed time: //' -e 's/ seconds//')
-# 		echo "$CATEGORY: $TIME" >> "../$RESULT_FILE"
-# 	done
-# 	echo ---$'\n' >> "../$RESULT_FILE"
-# 	cd ..
+#     cd $EX
+#     if [ ! -e $DATA_FILE ]; then
+#         echo "no $DATA_FILE file found, skipping $EX..."
+#         cd ..
+#         continue
+#     fi
+#     echo ---$'\n'Results for model $EX >> "../$RESULT_FILE"
+#     
+#     for CATEGORY in "${CATEGORIES[@]}"; do
+#         TIME=$(grep -A 1 "$CATEGORY" $DATA_FILE | tail -1 | sed -e 's/elapsed time: //' -e 's/ seconds//')
+#         echo "$CATEGORY: $TIME" >> "../$RESULT_FILE"
+#     done
+#     echo ---$'\n' >> "../$RESULT_FILE"
+#     cd ..
 # done

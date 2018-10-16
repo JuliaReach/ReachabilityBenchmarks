47ca8280f569c2a0ae65c25aa0c4bceb4c4498ea
Unknown <kostemkin@gmail.com>
2018-10-16 19:50:56 +1100

    added initial files for HSCC2019 RE

---
+++ models/HSCC2019/re_hscc.jl
@@ -0,0 +1,45 @@
+using HybridSystems, MathematicalSystems, LazySets, Reachability, Polyhedra, Optim
+import LazySets.HalfSpace
+include("FilteredOscillator.jl")
+
+N = Float64
+
+opDs = [Reachability.ReachSets.TextbookDiscretePost(),
+            Reachability.ReachSets.LazyTextbookDiscretePost(),
+            Reachability.ReachSets.ApproximatingDiscretePost(
+                Options(:overapproximation=>Hyperrectangle))];
+
+#warmup run for each opD for low dimension
+println("Warmup run just after restart REPL")
+for opD_i in 1:length(opDs)
+    opD = opDs[opD_i]
+    sol = filtered_oscillator(2, opDs[opD_i]);
+end
+println("End of warmup run")
+
+results = Vector{Tuple{AbstractSolution, Int64}}()
+for opD_i in 1:length(opDs)
+    println("**********************************")
+    println(opDs[opD_i])
+    upper_bound = opD_i == 3 ? 16 : 4;
+    n0 = 2;
+    while (n0 <= upper_bound)
+        println("\t", n0)
+        opD = opDs[opD_i];
+        sol = filtered_oscillator(n0, opD);
+        sol_proj = get_projection(sol, n0+2);
+        push!(results, (sol, opD_i));
+        n0 *= 2;
+    end
+end
+
+function get_projection(sol::AbstractSolution, system_dimension::Int64) ::AbstractSolution
+    sol_processed =  Reachability.ReachSolution([Reachability.ReachSet{CartesianProductArray{N}, N}(
+                CartesianProductArray{N, HPolytope{N}}([LazySets.Approximations.overapproximate(rs.X, LazySets.Approximations.OctDirections(system_dimension))]),
+                rs.t_start, rs.t_end) for rs in sol.Xk], sol.options)
+
+    sol_proj = Reachability.ReachSolution(Reachability.project_reach(
+        sol_processed.Xk, [1,2], system_dimension, sol.options), sol.options);
+
+    return sol_proj;
+end

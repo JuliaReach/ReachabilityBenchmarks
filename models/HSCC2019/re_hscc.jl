22652e23d2edc4969f5642413c60fa7c44ea0e74
Unknown <kostemkin@gmail.com>
2018-10-18 03:37:26 +1100

    fixed projection

--- models/HSCC2019/re_hscc.jl
+++ models/HSCC2019/re_hscc.jl
@@ -4,6 +4,16 @@ include("FilteredOscillator.jl")
 
 N = Float64
 
+function get_projection(sol::AbstractSolution, system_dimension::Int64) ::AbstractSolution
+    sol_processed =  Reachability.ReachSolution([Reachability.ReachSet{CartesianProductArray{N}, N}(
+                CartesianProductArray{N, HPolytope{N}}([LazySets.Approximations.overapproximate(rs.X, LazySets.Approximations.OctDirections(system_dimension))]),
+                rs.t_start, rs.t_end) for rs in sol.Xk], sol.options)
+
+    sol_proj = Reachability.ReachSolution(Reachability.project_reach(
+        sol_processed.Xk, [1,2], system_dimension, sol.options), sol.options);
+
+    return sol_proj;
+end
 opDs = [Reachability.ReachSets.TextbookDiscretePost(),
             Reachability.ReachSets.LazyTextbookDiscretePost(),
             Reachability.ReachSets.ApproximatingDiscretePost(
@@ -59,8 +69,8 @@ for opD_i in 1:length(opDs)
         println("\t", n0)
         opD = opDs[opD_i];
         sol = filtered_oscillator(n0, opD, t, max_jumps);
-        #sol_proj = get_projection(sol, n0+2);
-        push!(results, (sol, opD_i));
+        sol_proj = get_projection(sol, n0+2);
+        push!(results, (sol_proj, opD_i));
         if n0 == 128
             n0 = 196
         elseif n0 == 196
@@ -69,15 +79,4 @@ for opD_i in 1:length(opDs)
             n0 *= 2;
         end
     end
-end
-
-function get_projection(sol::AbstractSolution, system_dimension::Int64) ::AbstractSolution
-    sol_processed =  Reachability.ReachSolution([Reachability.ReachSet{CartesianProductArray{N}, N}(
-                CartesianProductArray{N, HPolytope{N}}([LazySets.Approximations.overapproximate(rs.X, LazySets.Approximations.OctDirections(system_dimension))]),
-                rs.t_start, rs.t_end) for rs in sol.Xk], sol.options)
-
-    sol_proj = Reachability.ReachSolution(Reachability.project_reach(
-        sol_processed.Xk, [1,2], system_dimension, sol.options), sol.options);
-
-    return sol_proj;
 end

b9072707f3ac081e55f8abb70ee88ea3ac1d49ae
schillic <schillic@informatik.uni-freiburg.de>
2018-10-18 09:37:27 +0200

    add bound variable

--- models/HSCC2019/FilteredOscillator.jl
+++ models/HSCC2019/FilteredOscillator.jl
@@ -3,8 +3,8 @@
 # ============================
 
 function filtered_oscillator(n0, opD, t, max_jumps)::AbstractSolution
-    system_dimension = n0 + 2
-    z = zeros(n0)
+    system_dimension = n0 + 3
+    z = zeros(n0+1)
 
     # Transition graph (automaton)
     a = LightAutomaton(4);
@@ -17,8 +17,8 @@ function filtered_oscillator(n0, opD, t, max_jumps)::AbstractSolution
     A = zeros(system_dimension, system_dimension)
     A[1,1], A[2,2] = -2., -1.
     A[3,1], A[3,3] = 5., -5.
-    for i = 4 : system_dimension
-    A[i,i-1], A[i,i] = 5., -5.
+    for i = 4 : system_dimension-1
+        A[i,i-1], A[i,i] = 5., -5.
     end
 
     # common U
@@ -46,7 +46,8 @@ function filtered_oscillator(n0, opD, t, max_jumps)::AbstractSolution
     #Mode 4
     B = [-1.4; 0.7; z]
     X = HPolyhedron([HalfSpace([0.714286; 1.0; z], 0.0),  # 0.714286*x + y <= 0
-               HalfSpace([-1.0; 0.0; z], 0.0)])  # x >= 0
+               HalfSpace([-1.0; 0.0; z], 0.0),  # x >= 0
+               HalfSpace([zeros(system_dimension-1); 1.], 2.0)])  # k <= 2
     m_4 = ConstrainedLinearControlContinuousSystem(A, eye(size(B, 1)), X, B*U);
 
     m = [m_1, m_2, m_3, m_4];
@@ -60,7 +61,9 @@ function filtered_oscillator(n0, opD, t, max_jumps)::AbstractSolution
     X_l3l4 = HPolyhedron([HalfSpace([-1.0; 0.0; z], 0.0),  # x >= 0
                HalfSpace([-0.714286; -1.0; z], 0.0),  # 0.714286*x + y >= 0
                HalfSpace([0.714286; 1.0; z], 0.0)])  # 0.714286*x + y <= 0
-    r1 = ConstrainedLinearDiscreteSystem(A_trans, X_l3l4);
+    A_trans_34 = eye(system_dimension)
+    A_trans_34[system_dimension, system_dimension] = 2.
+    r1 = ConstrainedLinearDiscreteSystem(A_trans_34, X_l3l4);
     # Transition l4 -> l2
     X_l4l2 = HPolyhedron([HalfSpace([0.714286; 1.0; z], 0.0),  # 0.714286*x + y <= 0
                HalfSpace([-1.0; 0.0; z], 0.0),  # x >= 0
@@ -84,13 +87,14 @@ function filtered_oscillator(n0, opD, t, max_jumps)::AbstractSolution
     HS = HybridSystem(a, m, r, s);
 
     # initial condition in mode 1
-    X0 = Hyperrectangle(low=[0.2; -0.1; zeros(n0)],
-                    high=[0.3; 0.1; zeros(n0)]);
+    X0 = Hyperrectangle(low=[0.2; -0.1; zeros(n0); 1.0],
+                    high=[0.3; 0.1; zeros(n0); 1.0]);
 
     system = InitialValueProblem(HS, [(3, X0)]);
     plot_vars = [1, 2]
-    options = Options(:mode=>"reach",:vars=>1:system_dimension, :T=>t, :δ=>0.01, :max_jumps=>max_jumps,
-                  :plot_vars=>plot_vars, :ε_proj=>0.001, :verbosity=>0, :project_reachset=>false);
+    options = Options(:mode=>"reach",:vars=>1:system_dimension, :T=>t, :δ=>0.01,
+                      :max_jumps=>max_jumps, :plot_vars=>plot_vars,
+                      :ε_proj=>0.001, :verbosity=>0, :project_reachset=>false);
 
 
     # default algorithm

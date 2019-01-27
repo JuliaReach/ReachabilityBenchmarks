12750d98a4bef7f4e038de2fd15360d85b729c0f
schillic <schillic@informatik.uni-freiburg.de>
2019-01-27 11:40:57 +0100

    fix cut-away axis labels; change colors figure 5

--- models/HSCC2019/create_figure_2.jl
+++ models/HSCC2019/create_figure_2.jl
@@ -43,8 +43,9 @@ for (i, ε) in enumerate(εs)
 end
 
 plot(res[1], color=:lightblue, opacity=0.5,
-             tickfont=font(20, "Times"),
-             guidefontsize=30, xlab=L"x_1", ylab=L"x_{50}",
+             tickfont=font(20, "Times"), guidefontsize=30,
+             xlab=L"x_1\raisebox{-1mm}{\textcolor{white}{.}}",
+             ylab=L"x_{50}\raisebox{2mm}{\textcolor{white}{.}}",
              xtick=[0.4, 0.8, 1.2], ytick=[0.8, 1.0, 1.2],
              bottom_margin=8mm, left_margin=8mm)
 plot!(res[2], color=:green, opacity=0.5)
--- models/HSCC2019/create_figure_5.jl
+++ models/HSCC2019/create_figure_5.jl
@@ -17,14 +17,23 @@ sol_proj4 = get_projection(sol4, [1, 6])
 sol16 = filtered_oscillator(16, ApproximatingDiscretePost(), 99.)
 sol_proj16 = get_projection(sol16, [1, 18])
 
-plot(sol_proj4, tickfont=font(20, "Times"), guidefontsize=30, xlab=L"x", ylab=L"x_4",
+color = RGB(0.1, 0.2, 1.0)
+linecolor = RGBA(1.0, 1.0, 1.0, 0.1)
+
+plot(sol_proj4, color=color, linecolor=linecolor,
+                tickfont=font(20, "Times"), guidefontsize=30,
+                xlab=L"x\raisebox{-1mm}{\textcolor{white}{.}}",
+                ylab=L"x_4\raisebox{2mm}{\textcolor{white}{.}}",
                 xtick=[-0.5, 0.0, 0.5], ytick=[-0.5, 0.0, 0.5],
-                bottom_margin=8mm, left_margin=8mm)
+                bottom_margin=8mm, left_margin=6mm)
 savefig("Figure 5 Left.pdf")
 
-plot(sol_proj16, tickfont=font(20, "Times"), guidefontsize=30, xlab=L"x", ylab=L"x_{16}",
-                xtick=[-0.5, 0.0, 0.5], ytick=[0.0, 0.2, 0.4],
-                bottom_margin=8mm, left_margin=8mm)
+plot(sol_proj16, color=color, linecolor=linecolor,
+                 tickfont=font(20, "Times"), guidefontsize=30,
+                 xlab=L"x\raisebox{-1mm}{\textcolor{white}{.}}",
+                 ylab=L"x_{16}\raisebox{2mm}{\textcolor{white}{.}}",
+                 xtick=[-0.5, 0.0, 0.5], ytick=[0.0, 0.2, 0.4],
+                 bottom_margin=8mm, left_margin=6mm)
 savefig("Figure 5 Right.pdf")
 
 #

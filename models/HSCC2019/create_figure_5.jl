5a55499d08886b143490b3a79dc46b28aef81a43
mforets <mforets@gmail.com>
2019-01-27 03:56:02 -0300

    fix plot labels

--- models/HSCC2019/create_figure_5.jl
+++ models/HSCC2019/create_figure_5.jl
@@ -26,20 +26,21 @@ sol_proj4 = get_projection(sol4, [1, 6])
 sol16 = filtered_oscillator(16, ApproximatingDiscretePost(), 99.)
 sol_proj16 = get_projection(sol16, [1, 18])
 
-plot(sol_proj4, tickfont=font(20, "Times"), guidefontsize=30, xlab=L"x_1", ylab=L"x_6",
+plot(sol_proj4, tickfont=font(20, "Times"), guidefontsize=30, xlab=L"x", ylab=L"x_4",
                 xtick=[-0.5, 0.0, 0.5], ytick=[-0.5, 0.0, 0.5],
                 bottom_margin=8mm, left_margin=8mm)
 savefig("Figure 5 Left.pdf")
 
-plot(sol_proj16, tickfont=font(20, "Times"), guidefontsize=30, xlab=L"x_1", ylab=L"x_{18}",
+plot(sol_proj16, tickfont=font(20, "Times"), guidefontsize=30, xlab=L"x", ylab=L"x_{16}",
                 xtick=[-0.5, 0.0, 0.5], ytick=[0.0, 0.2, 0.4],
                 bottom_margin=8mm, left_margin=8mm)
 savefig("Figure 5 Right.pdf")
 
 #
 # Commands useful for plotly()
-# plot(sol_proj4, tickfont=font(18, "Times"), guidefontsize=18)
-# plot(sol_proj16, tickfont=font(18, "Times"), guidefontsize=18)
+# plot(sol_proj4, tickfont=font(18, "Times"), guidefontsize=18, xlab="x", ylab="x₄")
+#x16 = "x"*join(Char.(0x2080 .+ convert.(UInt16, [1, 6])))
+# plot(sol_proj16, tickfont=font(18, "Times"), guidefontsize=18, xlab="x", ylab=x16)
 # The figures are opened in a browser tab; then click on the "Save as png" button.
 #
 
@@ -47,8 +48,8 @@ savefig("Figure 5 Right.pdf")
 # Commands useful for pyplot()
 # Plots.scalefontsizes() # restart to default values
 # Plots.scalefontsizes(4) # set font sizes x4 bigger
-# plot(sol_proj4)
+# plot(sol_proj4) or plot(sol_proj4, xlab="x", ylab="x₄")
 # savefig("Figure 5 Left.pdf")
-# plot(sol_proj16)
+# plot(sol_proj16) or plot(sol_proj4, xlab="x", ylab=x16)
 # savefig("Figure 5 Right.pdf")
 #

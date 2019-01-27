0f68698995322237f63b4fd50689493226850b57
mforets <mforets@gmail.com>
2019-01-27 03:37:41 -0300

    more tweaks in Fig 5

--- models/HSCC2019/create_figure_5.jl
+++ models/HSCC2019/create_figure_5.jl
@@ -3,10 +3,14 @@
 #
 # julia> include("create_figure_5.jl")
 #
+# By default, this script requires that you have installed the GR Plots backend.
+# See the bottom of this file for recommended setups for other plotting backends.
 # =============================================================================
-using Plots, LaTeXStrings
+using Plots
+using LaTeXStrings         # to write math labels using GR
+using Plots.PlotMeasures   # to specify margins units such as bottom_margin=8mm
 
-# set the plotting backend among: pyplot(), plotly(), gr()
+# select a plotting backend among: pyplot(), plotly(), gr()
 gr()
 
 if Plots.backend() == Plots.GRBackend()
@@ -22,10 +26,14 @@ sol_proj4 = get_projection(sol4, [1, 6])
 sol16 = filtered_oscillator(16, ApproximatingDiscretePost(), 99.)
 sol_proj16 = get_projection(sol16, [1, 18])
 
-plot(sol_proj4, tickfont=font(18, "Times"), guidefontsize=18, xlab=L"x_1", ylab=L"x_2")
+plot(sol_proj4, tickfont=font(20, "Times"), guidefontsize=30, xlab=L"x_1", ylab=L"x_6",
+                xtick=[-0.5, 0.0, 0.5], ytick=[-0.5, 0.0, 0.5],
+                bottom_margin=8mm, left_margin=8mm)
 savefig("Figure 5 Left.pdf")
 
-plot(sol_proj16, tickfont=font(18, "Times"), guidefontsize=18, xlab=L"x_1", ylab=L"x_2")
+plot(sol_proj16, tickfont=font(20, "Times"), guidefontsize=30, xlab=L"x_1", ylab=L"x_{18}",
+                xtick=[-0.5, 0.0, 0.5], ytick=[0.0, 0.2, 0.4],
+                bottom_margin=8mm, left_margin=8mm)
 savefig("Figure 5 Right.pdf")
 
 #

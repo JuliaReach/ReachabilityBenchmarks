042436a94b7ef708a99abe2ab1dfc4b53d1108c0
mforets <mforets@gmail.com>
2019-01-27 01:27:45 -0300

    use LaTeXStrings

--- models/HSCC2019/create_figure_5.jl
+++ models/HSCC2019/create_figure_5.jl
@@ -4,19 +4,43 @@
 # julia> include("create_figure_5.jl")
 #
 # =============================================================================
-using Plots
+using Plots, LaTeXStrings
 
-# disable graphics output in GR - https://github.com/JuliaPlots/Plots.jl/issues/1182
-# ENV["GKSwstype"] = "100"
+# set the plotting backend among: pyplot(), plotly(), gr()
+gr()
+
+if Plots.backend() == Plots.GRBackend()
+    # disable graphics output in GR - https://github.com/JuliaPlots/Plots.jl/issues/1182
+    ENV["GKSwstype"] = "100"
+end
 
 include("FilteredOscillator.jl")
 
 sol4 = filtered_oscillator(4, ApproximatingDiscretePost(), 20.)
 sol_proj4 = get_projection(sol4, [1, 6])
-plot(sol_proj4, tickfont=font(18, "Times"), guidefontsize=18)
-#savefig("Figure 5 Left.pdf")
 
 sol16 = filtered_oscillator(16, ApproximatingDiscretePost(), 99.)
 sol_proj16 = get_projection(sol16, [1, 18])
-plot(sol_proj16, tickfont=font(18, "Times"), guidefontsize=18)
-#savefig("Figure 5 Right.pdf")
+
+plot(sol_proj4, tickfont=font(18, "Times"), guidefontsize=18, xlab=L"x_1", ylab=L"x_2")
+savefig("Figure 5 Left.pdf")
+
+plot(sol_proj16, tickfont=font(18, "Times"), guidefontsize=18, xlab=L"x_1", ylab=L"x_2")
+savefig("Figure 5 Right.pdf")
+
+#
+# Commands useful for plotly()
+# plot(sol_proj4, tickfont=font(18, "Times"), guidefontsize=18)
+# plot(sol_proj16, tickfont=font(18, "Times"), guidefontsize=18)
+# The figures are opened in a browser tab; then click on the "Save as png" button.
+#
+
+#
+# Commands useful for pyplot()
+# Plots.scalefontsizes() # restart to default values
+# Plots.scalefontsizes(4) # set font sizes x4 bigger
+# plot(sol_proj4)
+# savefig("Figure 5 Left.pdf")
+# plot(sol_proj16)
+# savefig("Figure 5 Right.pdf")
+#

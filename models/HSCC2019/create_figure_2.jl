377b6942e18216561ec7a1e07abec0d2a26d116d
schillic <schillic@informatik.uni-freiburg.de>
2019-01-27 13:33:54 +0100

    code-cleanup figure 2

--- models/HSCC2019/create_figure_2.jl
+++ models/HSCC2019/create_figure_2.jl
@@ -26,20 +26,21 @@ else
 end
 
 n = 1000; m = 2; δ = 0.1;
-A, B = sprandn(n, n, 0.01), randn(n, m);
-X0 = BallInf(ones(n), 0.1);
-U = Ball2(zeros(m), 1.2);
-Y = ConvexHull(SparseMatrixExp(A * δ) * X0 ⊕ δ * B *U, X0);
-π = spzeros(2, n) ; π[1, 1] = π[2, 50] = 1;
+A = sprandn(n, n, 0.01); B = randn(n, m)
+X0 = BallInf(ones(n), 0.1)
+U = Ball2(zeros(m), 1.2)
+Y = ConvexHull(SparseMatrixExp(A * δ) * X0 ⊕ δ * B * U, X0)
+π = spzeros(2, n); π[1, 1] = π[2, 50] = 1
+
 res = Vector{HPolygon{Float64}}(undef, 3)
-εs = [Inf, 0.1, 0.01]
+εs = [Inf, 0.1, 0.001]
 println("warm-up runtimes:")
 for (i, ε) in enumerate(εs)
-    @time res[i] = overapproximate(π * Y, ε);
+    @time res[i] = overapproximate(π * Y, ε)
 end
 println("runtimes to obtain an overapproximation from the projection:")
 for (i, ε) in enumerate(εs)
-    @time res[i] = overapproximate(π * Y, ε);
+    @time res[i] = overapproximate(π * Y, ε)
 end
 
 plot(res[1], color=:lightblue, opacity=0.5,

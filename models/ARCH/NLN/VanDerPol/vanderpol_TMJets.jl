272e68cdcf593f5be9b17d82aa531a1c1e48121b
mforets <mforets@gmail.com>
2019-02-20 10:29:38 -0300

    change default values

--- models/ARCH/NLN/VanDerPol/vanderpol_TMJets.jl
+++ models/ARCH/NLN/VanDerPol/vanderpol_TMJets.jl
@@ -23,17 +23,17 @@ end
 - `t0`       -- (optional, default: `0.0`) initial time
 - `T`        -- (optional, default: `7.0`) time horizon
 - `abs_tol`  -- (optional, default: `1e-20`) absolute tolerance used for time step
-- `orderT`   -- (optional, default: `18`) order of the Taylor model in t
+- `orderT`   -- (optional, default: `13`) order of the Taylor model in t
 - `orderQ`   -- (optional, default: `9`) order of the Taylor model for Jet transport
                 variables
-- `maxsteps` -- (optional, default: `200`) use this maximum number of steps in
+- `maxsteps` -- (optional, default: `500`) use this maximum number of steps in
                 the validated integration
 - `float_coeffs` -- (optional, default: `true`) if `true`, use floating point numbers
                     for the coefficients of the polynomial variables; otherwise
                     use intervals
 """
-function vanderpol_TMJets(; t0=0.0, T=7.0, abs_tol=1e-20, orderT=18, orderQ=9,
-                            maxsteps=200, float_coeffs=true)
+function vanderpol_TMJets(; t0=0.0, T=7.0, abs_tol=1e-20, orderT=13, orderQ=9,
+                            maxsteps=500, float_coeffs=true)
 
     # Initial conditions as mid-point of provided intervals
     q0 = IntervalBox(1.4, 2.4)
@@ -47,7 +47,7 @@ function vanderpol_TMJets(; t0=0.0, T=7.0, abs_tol=1e-20, orderT=18, orderQ=9,
     δq0 = IntervalBox(-0.15..0.15, -0.05..0.05)
 
     # returns a TaylorN vector, each entry corresponding to an indep variable
-    #δ₁, δ₂ = set_variables("δ", numvars=length(q0), order=2*orderQ)
+    set_variables("x", numvars=2, order=2*orderQ)
 
     # TODO: wrap as a Reachability algorithm
     tTM, xTM = validated_integ(vanderPol!, q0, δq0, t0, T, orderQ, orderT, abs_tol, maxsteps=maxsteps)

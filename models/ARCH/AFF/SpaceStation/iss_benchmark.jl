2ac905d88953a4b68e17072c0b0aa7a7acff2f28
mforets <mforets@gmail.com>
2019-02-21 09:08:31 -0300

    update iss benchmark

--- models/ARCH/AFF/SpaceStation/iss_benchmark.jl
+++ models/ARCH/AFF/SpaceStation/iss_benchmark.jl
@@ -7,24 +7,14 @@ include("iss_BFFPSV18.jl")
 # Execute benchmarks and save benchmark results
 # ==============================================================================
 
-#=
-Uncomment to re-use parameters file
+# tune parameters
+tune!(SUITE)
 
-# From BenchmarkTools.jl
-# If a cache of tuned parameters already exists, use it, otherwise, tune and cache
-# the benchmark parameters. Reusing cached parameters is faster and more reliable
-# than re-tuning `suite` every time the file is included.
-paramspath = joinpath(dirname(@__FILE__), "params.json")
-
-if isfile(paramspath)
-    loadparams!(SUITE, BenchmarkTools.load(paramspath)[1], :evals);
-else
-    tune!(SUITE)
-    BenchmarkTools.save(paramspath, params(SUITE));
-end
-=#
-
+# run the benchmarks
 results = run(SUITE, verbose=true)
 
-# return the samples with the smallest time value in each test
+# return the sample with the smallest time value in each test
 println(minimum(results))
+
+# return the mean for each test
+println(mean(results))

eb7075c7e8bae896f6665a27d09d680d779080d6
mforets <mforets@gmail.com>
2019-03-24 07:47:17 -0300

    add ARCH COMP NLN script

---
+++ models/ARCH/NLN/ARCH-COMP_2019.jl
@@ -0,0 +1,10 @@
+# Van der Pol benchmark
+include("VanDerPol/vanderpol_benchmark.jl")
+
+# Laub-Loomis benchmark
+include("LaubLoomis/laubloomis_benchmark.jl")
+
+# Quadrotor benchmark
+include("Quadrotor/quad_benchmark.jl")
+
+nothing

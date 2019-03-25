c9d4383e04aa8c8c7d191e314ec8611d49f3b91f
mforets <mforets@gmail.com>
2019-03-25 01:40:56 -0300

    update options

--- models/ARCH/NLN/SpacecraftRendezvous/spacecraft_rendezvous.jl
+++ models/ARCH/NLN/SpacecraftRendezvous/spacecraft_rendezvous.jl
@@ -152,8 +152,11 @@ function spacecraft_rendezvous(;T=200.0, orderT=10, orderQ=2, abs_tol=1e-10,
     return 𝑃, 𝑂, 𝑂jets
 end
 
-𝑃, 𝑂, 𝑂jets = spacecraft_rendezvous(T=200.0, orderT=14, orderQ=6, abs_tol=1e-28, max_steps=5000);
+𝑃, 𝑂, 𝑂jets = spacecraft_rendezvous(T=200.0, orderT=10, orderQ=2, abs_tol=1e-28, max_steps=5000);
 sol = solve(𝑃, 𝑂, TMJets(𝑂jets), LazyDiscretePost(:check_invariant_intersection=>true))
 
+# first mode
+#p = IVP(𝑃.s.modes[1], 𝑃.x0[1][2])
+#solve(p, 𝑂, op=TMJets(𝑂jets))
 # compute projection onto the plot variables (project_reachset doesn't currently
 # work for a limitation in the hybrid solve, which does not preserve the type of opC)

bfcbfd7f2293eab9dcd06e6cc1e7591f453a5c91
mforets <mforets@gmail.com>
2019-03-29 16:38:17 -0300

    fix final plot

--- models/ARCH/NLN/VanDerPol/vanderpol_benchmark.jl
+++ models/ARCH/NLN/VanDerPol/vanderpol_benchmark.jl
@@ -32,22 +32,34 @@ SUITE["VanDerPol"]["μ = 1: x[2] <= 2.75"] = @benchmarkable solve($𝑃, $𝑂, 
 # Case 2: μ = 2
 # ----------------------------------------
 
-# benchmark settings
-𝑃, 𝑂 = vanderpol(μ=2, T=8.0,
-                 X0=Hyperrectangle(low=[1.55, 2.35], high=[1.85, 2.45]),
-                 property=(t,x) -> x[2] < 4.0)
+X0_μ2 = Hyperrectangle(low=[1.55, 2.35], high=[1.85, 2.45])
 
 # algorithm-specific options
-𝑂jets = Options(:abs_tol=>1e-10, :orderT=>10, :orderQ=>2, :max_steps=>500)
+𝑂jets = Options(:abs_tol=>1e-10, :orderT=>8, :orderQ=>2, :max_steps=>500)
 
-# first run
-sol_2 = solve(𝑃, 𝑂, op=TMJets(𝑂jets))
+# the idea is to split the initial states along the "x" direction
+# n controls the splitting
+nsplits_x = 8
 
-# verify that specification holds
-@assert all([ρ([0.0, 1.0], sol_2.Xk[i].X) < 4.0 for i in eachindex(sol_2.Xk)])
+function compute_μ2(;n::Int=8, validate=true)
+    sol_2 = []
+    for X0i in split(X0_μ2, n, 1)
+        𝑃, 𝑂 = vanderpol(μ=2.0, T=8.0, X0=X0i, property=(t,x) -> x[2] < 4.0)
+        𝑂jets = Options(:abs_tol=>1e-10, :orderT=>8, :orderQ=>1, :max_steps=>500)
+        sol_2_i = solve(𝑃, 𝑂, op=TMJets(𝑂jets))
+        push!(sol_2, sol_2_i)
+        if validate
+            # verify that specification holds
+            @assert all([ρ([0.0, 1.0], sol_2_i.Xk[i].X) < 4.0 for i in eachindex(sol_2_i.Xk)])
+        end
+    end
+    return sol_2
+end
+
+sol_2 = compute_μ2(n=nsplits_x, validate=true)
 
 # benchmark
-SUITE["VanDerPol"]["μ = 2: x[2] <= 4.0"] = @benchmarkable solve($𝑃, $𝑂, op=TMJets($𝑂jets))
+SUITE["VanDerPol"]["μ = 2: x[2] <= 4.0"] = @benchmarkable compute_μ2(n=$nsplits_x, validate=false)
 
 # ==============================================================================
 # Execute benchmarks and save benchmark results
@@ -69,6 +81,10 @@ println("median time for each benchmark:\n", median(results))
 # Create plots
 # ==============================================================================
 
+# --------------------------
+# Case 1
+# --------------------------
+
 plot(sol_1,
      tickfont=font(30, "Times"), guidefontsize=45,
      xlab=L"x_{1}\raisebox{-0.5mm}{\textcolor{white}{.}}",
@@ -81,14 +97,47 @@ plot(sol_1,
 plot!(x->x, x->2.75, -3., 3., line=2, color="red", linestyle=:dash, legend=nothing)
 savefig(@relpath "vanderpol_case_1.png")
 
-plot(sol_2,
-     tickfont=font(30, "Times"), guidefontsize=45,
-     xlab=L"x_{1}\raisebox{-0.5mm}{\textcolor{white}{.}}",
-     ylab=L"x_{2}\raisebox{2mm}{\textcolor{white}{.}}",
-     xtick=[-3., -2., -1., 0., 1., 2., 3.], ytick=[-3., -2., -1., 0., 1., 2., 3.],
-     xlims=(-3., 3.), ylims=(-3., 3.),
-     bottom_margin=6mm, left_margin=2mm, right_margin=4mm, top_margin=3mm,
-     size=(1000, 1000), linecolor="blue")
+# --------------------------
+# Case 2
+# --------------------------
 
-plot!(x->x, x->4.0, -3., 3., line=2, color="red", linestyle=:dash, legend=nothing)
-savefig(@relpath "vanderpol_case_2.png")
+plot_2 = plot(x->x, x->4.0, -2.5, 3., line=2, color="red", linestyle=:dash, legend=nothing)
+
+for i in 1:nsplits_x
+    plot!(plot_2, sol_2[i], tickfont=font(30, "Times"), guidefontsize=45,
+                   xlab=L"x_{1}\raisebox{-0.5mm}{\textcolor{white}{.}}",
+                   ylab=L"x_{2}\raisebox{2mm}{\textcolor{white}{.}}",
+                   xtick=[-2., -1., 0., 1., 2., 3.], ytick=[-4., -3., -2., -1., 0., 1., 2., 3., 4.],
+                   xlims=(-2.5, 3.), ylims=(-4.5, 4.),
+                   bottom_margin=6mm, left_margin=2mm, right_margin=4mm, top_margin=3mm,
+                   size=(1000, 1000), color="red", linewidth=0.0, linecolor="red", alpha=.5)
+end
+
+savefig(plot_2, @relpath "vanderpol_case_2.png")
+
+# --------------------------
+# Cases 1 and 2 overlapped
+# --------------------------
+
+plot_all = plot(x->x, x->2.75, -2.5, 3., line=2, color="red", linestyle=:dash, legend=nothing)
+plot!(plot_all, x->x, x->4.0, -2.5, 3., line=2, color="red", linestyle=:dash, legend=nothing)
+
+for i in 1:nsplits_x
+    plot!(plot_all, sol_2[i], tickfont=font(30, "Times"), guidefontsize=45,
+                   xlab=L"x_{1}\raisebox{-0.5mm}{\textcolor{white}{.}}",
+                   ylab=L"x_{2}\raisebox{2mm}{\textcolor{white}{.}}",
+                   xtick=[-2., -1., 0., 1., 2., 3.], ytick=[-4., -3., -2., -1., 0., 1., 2., 3., 4.],
+                   xlims=(-2.5, 3.), ylims=(-4.5, 4.),
+                   bottom_margin=6mm, left_margin=2mm, right_margin=4mm, top_margin=3mm,
+                   size=(1000, 1000), color="red", linewidth=0.0, linecolor="red", alpha=.5)
+end
+
+plot!(sol_1, tickfont=font(30, "Times"), guidefontsize=45,
+                       xlab=L"x_{1}\raisebox{-0.5mm}{\textcolor{white}{.}}",
+                       ylab=L"x_{2}\raisebox{2mm}{\textcolor{white}{.}}",
+                       xtick=[-2., -1., 0., 1., 2., 3.], ytick=[-3., -2., -1., 0., 1., 2., 3., 4.],
+                       xlims=(-2.5, 3.), ylims=(-4.5, 4.),
+                       bottom_margin=6mm, left_margin=2mm, right_margin=4mm, top_margin=3mm,
+                       size=(1000, 1000), color="blue", linewidth=1., linecolor="blue", alpha=.8)
+
+savefig(plot_all, "vanderpol_case_all.png")

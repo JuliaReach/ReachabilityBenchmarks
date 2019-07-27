f579d5169ef6f64bbdd7a9bb778c88469de44ab0
Aadesha <aadeshdeshmukh25@gmail.com>
2019-07-27 13:54:06 +0530

    Update transmissionlinecircuits.jl

--- models/TransmissionLineCircuit/transmissionlinecircuits.jl
+++ models/TransmissionLineCircuit/transmissionlinecircuits.jl
@@ -2,15 +2,12 @@
 
 # ==============================================================================
 
-using Revise, MathematicalSystems, Reachability, LinearAlgebra, HybridSystems
-using LazySets
-using Reachability: solve
+using Reachability, HybridSystems
 using Plots
-using TaylorIntegration
 
 @taylorize function transmission_line_circuits_one!(t, x, dx)
    local α = 5
-   dx[1] = -2*x[1] + x[2] + 2 - exp(α*x[1]) - exp(α*(x[1] - x[2])) + 2
+   dx[1] = -2*x[1] + x[2] + 2 - exp(α*x[1]) - exp(α*(x[1] - x[2])) + x[7]
    dx[2] = -2*x[2] + x[1] + x[3] + exp(α*(x[1] - x[2])) - exp(α*(x[2] - x[3])) 
    dx[3] = -2*x[3] + x[2] + x[4] + exp(α*(x[2] - x[3])) - exp(α*(x[3] - x[4]))
    dx[4] = -2*x[4] + x[3] + x[5] + exp(α*(x[3] - x[4])) - exp(α*(x[4] - x[5]))
@@ -21,7 +18,7 @@ end
 
 @taylorize function transmission_line_circuits_two!(t, x, dx)
    local α = 5
-   dx[1] = -2*x[1] + x[2] + 2 - exp(α*x[1]) - exp(α*(x[1] - x[2])) + 3 - t
+   dx[1] = -2*x[1] + x[2] + 2 - exp(α*x[1]) - exp(α*(x[1] - x[2])) + x[7]
    dx[2] = -2*x[2] + x[1] + x[3] + exp(α*(x[1] - x[2])) - exp(α*(x[2] - x[3])) 
    dx[3] = -2*x[3] + x[2] + x[4] + exp(α*(x[2] - x[3])) - exp(α*(x[3] - x[4]))
    dx[4] = -2*x[4] + x[3] + x[5] + exp(α*(x[3] - x[4])) - exp(α*(x[4] - x[5]))
@@ -32,7 +29,7 @@ end
 
 @taylorize function transmission_line_circuits_three!(t, x, dx)
    local α = 5
-   dx[1] = -2*x[1] + x[2] + 2 - exp(α*x[1]) - exp(α*(x[1] - x[2])) + 1
+   dx[1] = -2*x[1] + x[2] + 2 - exp(α*x[1]) - exp(α*(x[1] - x[2])) + x[7]
    dx[2] = -2*x[2] + x[1] + x[3] + exp(α*(x[1] - x[2])) - exp(α*(x[2] - x[3])) 
    dx[3] = -2*x[3] + x[2] + x[4] + exp(α*(x[2] - x[3])) - exp(α*(x[3] - x[4]))
    dx[4] = -2*x[4] + x[3] + x[5] + exp(α*(x[3] - x[4])) - exp(α*(x[4] - x[5]))
@@ -45,26 +42,13 @@ function circuits()
 
    automaton = LightAutomaton(2) # two modes
 
-   inv_one = HPolyhedron([HalfSpace([1.0, 0.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 1.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 1.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 1.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 0.0, 1.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 0.0)]) 
+   inv_one = HPolyhedron([HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 1.0)]) 
 
-   inv_one = HPolyhedron([HalfSpace([1.0, 0.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 1.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 1.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 1.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 0.0, 1.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 0.0)])     
+   inv_two = HPolyhedron([HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 2.0), 
+                          HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0], -1.0)]) 
 
-   inv_one = HPolyhedron([HalfSpace([1.0, 0.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 1.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 1.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 1.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 0.0, 1.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 0.0)])                                                 
+   inv_three = HPolyhedron([HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0], -2.0)]) 
+                                                                                                                                          
 
    m1 = ConstrainedBlackBoxContinuousSystem(transmission_line_circuits_one!, 3, inv_one)
 
@@ -74,40 +58,42 @@ function circuits()
 
    modes = [m1, m2, m3]  #modes
 
-   G_first = HPolyhedron([HalfSpace([1.0, 0.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 1.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 1.0, 0.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 1.0, 0.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 0.0, 1.0, 0.0], 0.0),
-                          HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 0.0)])
 
-   G_second = HPolyhedron([HalfSpace([1.0, 0.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                           HalfSpace([0.0, 1.0, 0.0, 0.0, 0.0, 0.0], 0.0),
-                           HalfSpace([0.0, 0.0, 1.0, 0.0, 0.0, 0.0], 0.0),
-                           HalfSpace([0.0, 0.0, 0.0, 1.0, 0.0, 0.0], 0.0),
-                           HalfSpace([0.0, 0.0, 0.0, 0.0, 1.0, 0.0], 0.0),
-                           HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 0.0)])                          
-                          
+   add_transition!(automaton, 1, 2, 1)    # first transion
+   add_transition!(automaton, 2, 3, 2)    # second transion
+   add_transition!(automaton, 2, 1, 3)    # third transion
+   add_transition!(automaton, 3, 2, 4)    # fourth transion 
+     
+   G_first = HPolyhedron([HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0], -1.0)]) 
+   G_second = HPolyhedron([HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0], -2.0)]) 
+   G_third = HPolyhedron([HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 1.0)]) 
+   G_fourth = HPolyhedron([HalfSpace([0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0], 2.0)]) 
 
-   add_transition!(automaton, 1, 2, 1)    # first transition
-   add_transition!(automaton, 2, 3, 2)    # second transition
-
-   R_first = ConstrainedIdentityMap(6, G_first)
-   R_second = ConstrainedIdentityMap(6, G_second)
-
+   A1 = [0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 -1.0]
+   A2 = [0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0;
+        0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0; 0.0 0.0 0.0 0.0 0.0 0.0 0.0]
+        
+   
+   b1 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 2.0]
+   b2 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 3.0]
+   b3 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]
+   
+   R_first = ConstrainedAffineMap(A1, b2, G_first)
+   R_second = ConstrainedAffineMap(A2, b3, G_second)
+   R_third = ConstrainedAffineMap(A2, b1, G_third)
+   R_fourth = ConstrainedAffineMap(A1, b2, G_fourth)
    # resetmaps
-   resetmaps = [R_first, R_second]
-
+   resetmaps = [R_first, R_second, R_third, R_fourth]
    # switchings
    switching = AutonomousSwitching()
-   switchings = fill(switching, 2)
+   switchings = fill(switching, 4)
 
    ℋ = HybridSystem(automaton, modes, resetmaps, switchings)
-
    # initial condition in mode three
-   X0 = Hyperrectangle(low = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0], high = [0.02, 0.02, 0.02, 0.02, 0.02, 0.02])
-
-   initial_condition = [(1, X0)]  # initial condition in the "down" mode
+   X0 = Hyperrectangle(low = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], high = [0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.01])
+   # initial condition 
+   initial_condition = [(1, X0)]  
 
    system = InitialValueProblem(ℋ, initial_condition)
 

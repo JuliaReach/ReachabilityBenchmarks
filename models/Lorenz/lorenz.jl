77c66db0e5e381bf70108cff8422cfbb00926243
Sebastian Guadalupe <sebastianguadalupe@MacBook-Air-de-Sofia.local>
2019-10-21 19:20:07 -0300

    add lorenz

---
+++ models/Lorenz/lorenz.jl
@@ -0,0 +1,27 @@
+#=
+Lorenz model from
+https://en.wikipedia.org/wiki/Lorenz_system
+=#
+using TaylorIntegration
+using TaylorModels: @taylorize
+using Reachability
+using MathematicalSystems
+using LazySets
+
+@taylorize function lorenz!(dx, x, params, t)
+    local σ = 10.0
+    local β = 8.0 / 3.0
+    local ρ = 28.0
+    dx[1] = σ * (x[2] - x[1])
+    dx[2] = x[1] * (ρ - x[3]) - x[2]
+    dx[3] = x[1] * x[2] - β * x[3]
+    return dx
+end
+
+𝑆 = BlackBoxContinuousSystem(lorenz!, 3)
+X0 = Hyperrectangle(low=[0.9, 0.0, 0.0], high=[1.1, 0.0, 0.0])
+𝑃 = InitialValueProblem(𝑆, X0)
+
+# reach mode
+𝑂 = Options(:T=>10.0, :mode=>"reach")
+sol = solve(𝑃, 𝑂, op=TMJets(:abs_tol=>1e-27, :orderT=>10, :orderQ=>2));

f07f6ca83ba04364a4a3ab728cda6afad1a8ee2c
schillic <schillic@informatik.uni-freiburg.de>
2019-12-23 16:07:49 +0100

    update MNA1 model

---
+++ models/SLICOT/mna1/mna1_run.jl
@@ -0,0 +1,34 @@
+using ReachabilityBenchmarks, MathematicalSystems, Reachability, Plots
+
+include(@relpath "mna1_model.jl")
+include(@relpath "mna1_specifications.jl")
+
+S = mna1_model()
+X0, options = mna1_specification()
+
+# initial value problem
+problem = InitialValueProblem(S, X0)
+
+# partition with 1D blocks
+partition = [i:i for i in 1:578]
+
+# reachability algorithms
+algorithm_reach = BFFPSV18(:δ => 1e-3, :vars => [1], :partition => partition)
+algorithm_check = BFFPSV18(:δ => 4e-4, :vars => [1], :partition => partition)
+
+# check property
+options[:mode] = "check"
+solution = solve(problem, options; op=algorithm_check)
+@assert solution.satisfied
+
+# compute flowpipe
+options[:mode] = "reach"
+solution = solve(problem, options; op=algorithm_reach)
+
+# project flowpipe to time and x₁
+solution.options[:plot_vars] = [0, 1]
+solution_proj = project(solution)
+
+# plot projection
+plot(solution_proj)
+savefig("mna1")

edade220d09dbe94b40e51e1df67a8de190214e1
schillic <schillic@informatik.uni-freiburg.de>
2019-12-23 20:50:44 +0100

    update CD-Player model

---
+++ models/SLICOT/cdplayer/cdplayer_run.jl
@@ -0,0 +1,36 @@
+using ReachabilityBenchmarks, MathematicalSystems, Reachability, Plots
+
+include(@relpath "cdplayer_model.jl")
+include(@relpath "cdplayer_specifications.jl")
+
+S = cdplayer_model()
+X0, options = cdplayer_specification()
+
+# initial value problem
+problem = InitialValueProblem(S, X0)
+
+# partition with 1D blocks
+partition = [i:i for i in 1:120]
+
+# reachability algorithms
+algorithm_reach = BFFPSV18(:δ => 1e-3, :vars => [1], :partition => partition,
+                           :assume_sparse => true)
+algorithm_check = BFFPSV18(:δ => 1e-3, :vars => [1, 2], :partition => partition,
+                           :assume_sparse => true)
+
+# check property (does not verify for the given initial states)
+options[:mode] = "check"
+solution = solve(problem, options; op=algorithm_check)
+# @assert solution.satisfied
+
+# compute flowpipe
+options[:mode] = "reach"
+solution = solve(problem, options; op=algorithm_reach)
+
+# project flowpipe to time and x₁
+solution.options[:plot_vars] = [0, 1]
+solution_proj = project(solution)
+
+# plot projection
+plot(solution_proj)
+savefig("cdplayer")
---
+++ models/SLICOT/cdplayer/cdplayer_specifications.jl
@@ -0,0 +1,18 @@
+using LazySets, Reachability, SparseArrays
+
+function cdplayer_specification()
+    # initial set: xᵢ ∈ [-1, 1] for all i
+    X0 = BallInf(zeros(120), 1.0)
+
+    # safety property: 2·x₁ - 3·x₂ ≤ 450.8
+    property =
+        SafeStatesProperty(HalfSpace(sparsevec([1, 2], [2.0, -3.0], 120), 450.8))
+
+    # time horizon: 20 time units
+    time_horizon = 20.0
+
+    # specification
+    O = Options(:T => time_horizon, :property => property)
+
+    return X0, O
+end

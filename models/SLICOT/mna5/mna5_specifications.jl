4640e62cff99fe7d12692ddacef8526f7b2f4341
schillic <schillic@informatik.uni-freiburg.de>
2019-12-23 19:25:06 +0100

    update MNA5 model

---
+++ models/SLICOT/mna5/mna5_specifications.jl
@@ -0,0 +1,20 @@
+using LazySets, Reachability, SparseArrays
+
+function mna5_specification()
+    # initial set: xᵢ ∈ [0.0002, 0.00025] if i ≤ 10 and xᵢ = 0 otherwise
+    X0 = Hyperrectangle(low=[fill(0.0002, 10); zeros(10903)],
+                        high=[fill(0.00025, 10); zeros(10903)])
+
+    # property: x1 ≤ 0.2 && x2 ≤ 0.15
+    property = SafeStatesProperty(HPolyhedron([
+        HalfSpace(sparsevec([1], [1.0], 10913), 0.2),
+        HalfSpace(sparsevec([2], [1.0], 10913), 0.15)]))
+
+    # time horizon: 20 time units
+    time_horizon = 20.0
+
+    # specification
+    O = Options(:T => time_horizon, :property => property)
+
+    return X0, O
+end

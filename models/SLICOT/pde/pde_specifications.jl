246634e793ee9d0d010f19783123eb9b114b682b
schillic <schillic@informatik.uni-freiburg.de>
2019-12-23 14:24:48 +0100

    update PDE model

---
+++ models/SLICOT/pde/pde_specifications.jl
@@ -0,0 +1,22 @@
+using ReachabilityBenchmarks, LazySets, Reachability, MAT
+
+function pde_specification()
+    # initial set:
+    # xᵢ = 0 for i ≤ 64
+    # xᵢ ∈ [0.001, 0.0015] for i ∈ [65, 80]
+    # xᵢ ∈ [-0.002, -0.0015] for i > 80
+    X0 = Hyperrectangle(low=[zeros(64); fill(0.001, 16); fill(-0.002, 4)],
+                        high=[zeros(64); fill(0.0015, 16); fill(-0.0015, 4)])
+
+    # safety property: y ≤ 12 for linear combination y (defined in out.mat)
+    y = read(matopen(@relpath "out.mat"), "M")[1, :]
+    property = SafeStatesProperty(HalfSpace(y, 12.0))
+
+    # time horizon: 20 time units
+    time_horizon = 20.0
+
+    # specification
+    O = Options(:T => time_horizon, :property => property)
+
+    return X0, O
+end

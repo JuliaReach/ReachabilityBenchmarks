520a732351feb0b52813732cb60221bef14e073b
schillic <schillic@informatik.uni-freiburg.de>
2020-01-15 17:58:26 +0100

    fixes

--- models/TransmissionLine/transmission_line_run.jl
+++ models/TransmissionLine/transmission_line_run.jl
@@ -15,21 +15,20 @@ problem = InitialValueProblem(S, X0)
 # partition with 1D blocks
 partition = [i:i for i in 1:(2 * η)]
 
-# variables of interest: U_η ~ η, I_η ~ 2*η, U₁ ~ 1, I₁ ~ η+1, 
+# variables of interest: U₁ ~ 1, I₁ ~ η+1, U_η ~ η, I_η ~ 2*η
 vars = [1, η, η + 1, 2 * η]
 
 # reachability algorithm
-algorithm_reach = ASB07(:δ => 0.002, :max_order => 400,
-                        :order_discretization => 6, :partition => partition,
-                        :vars => vars)
+algorithm_reach = ASB07_decomposed(:δ => 0.002, :max_order => 400,
+    :order_discretization => 9, :partition => partition, :vars => vars)
 
 # compute flowpipe
 options[:mode] = "reach"
 solution = solve(problem, options; op=algorithm_reach)
 
 for (vars, suffix) in [([0, η], "t-U_n"),
-                       ([0, η], "U_1-I_n"),
-                       ([0, η], "U_n-I_n")]
+                       ([1, η + 1], "U_1-I_n"),
+                       ([η, η + 1], "U_n-I_n")]
     # project flowpipe to time and x_η
     solution.options[:plot_vars] = vars
     solution_proj = project(solution)

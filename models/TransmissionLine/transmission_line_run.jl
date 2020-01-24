042bd36975131ef3625c30787eb961253e58ca96
schillic <schillic@informatik.uni-freiburg.de>
2020-01-24 15:56:15 +0100

    make time step a variable

--- models/TransmissionLine/transmission_line_run.jl
+++ models/TransmissionLine/transmission_line_run.jl
@@ -20,16 +20,17 @@ partition = [1:(2 * η)]  # TODO temporarily use most precise partition
 vars = [1, η, η + 1, 2 * η]
 
 # reachability algorithm
-algorithm_reach = ASB07_decomposed(:δ => 0.002, :max_order => 400,
+δ = 0.002
+algorithm_reach = ASB07_decomposed(:δ => δ, :max_order => 400,
     :order_discretization => 9, :partition => partition, :vars => vars)
 # TODO ":order_discretization => 6" was used in the paper but that did not work
 # TODO temporary alternative with ASB07
-algorithm_reach2 = ASB07(:δ => 0.002, :max_order => 400,
+algorithm_reach2 = ASB07(:δ => δ, :max_order => 400,
     :order_discretization => 9)
 # TODO temporary alternative with BFFPSV18 without intervals
 problem3 = InitialValueProblem(ConstrainedLinearControlContinuousSystem(
     mid(S.A), mid(S.B), nothing, S.U), X0)
-algorithm_reach3 = BFFPSV18(:δ => 0.002, :partition => partition, :vars => vars)
+algorithm_reach3 = BFFPSV18(:δ => δ, :partition => partition, :vars => vars)
 
 # compute flowpipe
 options[:mode] = "reach"
@@ -69,7 +70,7 @@ for (vars, suffix) in [
     using SparseArrays
     if vars[1] == 0
         M = sparse([1], [vars[2]], [1.], 1, 2 * η)
-        Y = LazySets.Interval(0., algorithm_reach.options[:δ]) × (M * X0)
+        Y = LazySets.Interval(0., δ) × (M * X0)
     else
         M = sparse([1, 2], vars, [1., 1.], 2, 2 * η)
         Y = M * X0

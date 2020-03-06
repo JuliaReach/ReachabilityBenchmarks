1de6c0109ae44e894bacd9cbe36928b900ab4d9b
schillic <schillic@informatik.uni-freiburg.de>
2020-03-06 09:39:07 +0100

    update FiveDimSys model

---
+++ models/FiveDimSys/fiveDimSys_specifications.jl
@@ -0,0 +1,14 @@
+using LazySets
+
+function fiveDimSys_specification()
+    # initial set
+    X0 = BallInf([1.0, 0.0, 0.0, 0.0, 0.0], 0.1)
+
+    # time horizon: 5 time units
+    time_horizon = 5.0
+
+    # specification
+    O = Dict{Symbol, Any}(:T => time_horizon)
+
+    return X0, O
+end
